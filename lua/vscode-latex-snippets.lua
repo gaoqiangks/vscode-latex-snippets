2b18a51fb5bba910f76b56cd55fae3f159139f12
gaoqiang <gaoqiangks@qq.com>
2025-04-16 19:44:54 +0800

    a

--- lua/vscode-latex-snippets.lua
+++ lua/vscode-latex-snippets.lua
@@ -1,3 +1,8 @@
+-- local log=require("log").log
+local file_exists = function(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
 return {
     packages = {},
     reload_snippets = function()
@@ -7,12 +12,31 @@ return {
             return
         end
         local pkgs = vim.b.vimtex.packages
-        for pkg , _  in pairs(pkgs) do
+        local class = "class-"..vim.b.vimtex.documentclass
+        pkgs["_environments"] = 1
+        pkgs["_commands"] = 1
+        pkgs[class] = 1
+        -- log("pkgs= "..vim.inspect(pkgs))
+        for pkg, _  in pairs(pkgs) do
             if not vsc_latex.packages[pkg] then
                 vsc_latex.packages[pkg] = 1
                 local pkg_json = "/home/gaoqiang/.local/share/nvim/lazy/vscode-latex-snippets/snippets/"..pkg..".json"
-                require("luasnip.loaders.from_vscode").load_standalone({path = pkg_json})
+                if file_exists(pkg_json) then
+                    require("luasnip.loaders.from_vscode").load_standalone({path = pkg_json})
+                end
             end
         end
+    end,
+    setup = function()
+        local vsc_tex_snips = require("vscode-latex-snippets")
+        -- local vimtex = require("vimtex")
+        vim.api.nvim_create_autocmd("ModeChanged", {
+            pattern = "*:[nv]", -- 从插入模式切换到普通模式或可视模式
+            callback = vsc_tex_snips.reload_snippets
+        })
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VimtexEventInitPost",
+            callback = vsc_tex_snips.reload_snippets
+        })
     end
 }
