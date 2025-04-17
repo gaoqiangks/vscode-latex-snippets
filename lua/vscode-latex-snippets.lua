6ea38b17fa2806d74f51e4e92c1c64e0087423b6
gaoqiang <gaoqiangks@qq.com>
2025-04-18 06:18:45 +0800

    aa

--- lua/vscode-latex-snippets.lua
+++ lua/vscode-latex-snippets.lua
@@ -16,11 +16,12 @@ return {
         pkgs["_environments"] = 1
         pkgs["_commands"] = 1
         pkgs[class] = 1
+        local lazy_root = require("lazy.core.config").options.root
         -- log("pkgs= "..vim.inspect(pkgs))
         for pkg, _  in pairs(pkgs) do
             if not vsc_latex.packages[pkg] then
                 vsc_latex.packages[pkg] = 1
-                local pkg_json = "/home/gaoqiang/.local/share/nvim/lazy/vscode-latex-snippets/snippets/"..pkg..".json"
+                local pkg_json = lazy_root.."/vscode-latex-snippets/snippets/"..pkg..".json"
                 if file_exists(pkg_json) then
                     require("luasnip.loaders.from_vscode").load_standalone({path = pkg_json})
                 end
