8fec58fffda877c927b8924ecab2547149fd1c9c
gaoqiang <gaoqiangks@qq.com>
2025-04-27 01:33:19 +0800

    a

--- lua/vscode-latex-snippets.lua
+++ lua/vscode-latex-snippets.lua
@@ -1,7 +1,9 @@
--- local log=require("log").log
 local file_exists = function(name)
-    local f=io.open(name,"r")
-    if f~=nil then io.close(f) return true else return false end
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else return false end
 end
 return {
     packages = {},
@@ -12,18 +14,18 @@ return {
             return
         end
         local pkgs = vim.b.vimtex.packages
-        local class = "class-"..vim.b.vimtex.documentclass
+        local class = "class-" .. vim.b.vimtex.documentclass
         pkgs["_environments"] = 1
         pkgs["_commands"] = 1
         pkgs[class] = 1
         local lazy_root = require("lazy.core.config").options.root
         -- log("pkgs= "..vim.inspect(pkgs))
-        for pkg, _  in pairs(pkgs) do
+        for pkg, _ in pairs(pkgs) do
             if not vsc_latex.packages[pkg] then
                 vsc_latex.packages[pkg] = 1
-                local pkg_json = lazy_root.."/vscode-latex-snippets/snippets/"..pkg..".json"
+                local pkg_json = lazy_root .. "/vscode-latex-snippets/snippets/" .. pkg .. ".json"
                 if file_exists(pkg_json) then
-                    require("luasnip.loaders.from_vscode").load_standalone({path = pkg_json})
+                    require("luasnip.loaders.from_vscode").load_standalone({ path = pkg_json })
                 end
             end
         end
